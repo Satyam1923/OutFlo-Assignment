d5e1211191ce401b1a75bf815c6fcdfc80802c48
Satyam Rathor <satyamr1923@gmail.com>
2025-04-04 00:19:52 +0530

    remove extra files

--- src/routes/messageRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getPersonalizedMessage } from "../controllers/messageController";
-const router = express.Router();
-
-router.post('/',getPersonalizedMessage);
-
-export default router;
